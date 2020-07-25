9bf2f76b47c273f559cd2525477040840c2b5073
R Gorazda <rygorazd@microsoft.com>
2020-07-25 22:56:39 +0100

    master

--- HTML/booking.php
+++ HTML/booking.php
@@ -61,12 +61,12 @@
   <!-- main content-->
     <div class="container">
         <div class="wrapper">
-    <div class="container body-content" style="padding-top:35px";>
+    <!-- <div class="container body-content" style="padding-top:35px";> -->
 <h1> Booking page</h1>
     <?php include '3b-reserve-slot.php';?>
 
     </div>
-    </div>
+
 </div>
 
     <!-- footer -->
