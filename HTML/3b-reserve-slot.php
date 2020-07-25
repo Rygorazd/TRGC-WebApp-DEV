98f0033da5144ffdb445889c20507b0345e7d8d0
R Gorazda <rygorazd@microsoft.com>
2020-07-25 22:44:29 +0100

    master

--- HTML/3b-reserve-slot.php
+++ HTML/3b-reserve-slot.php
@@ -5,7 +5,7 @@
       PHP Reservation Demo - Single Time Slot Booking
     </title>
     <script src="/public/3b-reserve-slot.js"></script>
-    <link href="/public/3-theme.css" rel="stylesheet">
+    <link href="../CSS/3-theme.css" rel="stylesheet">
   </head>
   <body>
     <h1>
