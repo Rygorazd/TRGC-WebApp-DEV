008f517103ffd93a08ad4f2be0448701ef01c29a
R Gorazda <rygorazd@microsoft.com>
2020-07-29 22:21:22 +0100

    master

--- HTML/test.php
+++ HTML/test.php
@@ -10,20 +10,19 @@ table, th, td {
 <body>
 
 <?php
-$servername = "trgc-mysql.mysql.database.azure.com"; //host server 
-$username = "student";	//database username
-$password = "T0r0nt057";	//database password  
-$dbname = "trgcdev";	//database name
+require_once 'config.php';
+
+session_start();
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+//$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+//if ($conn->connect_error) {
+    //die("Connection failed: " . $conn->connect_error);
+//}
 
 $sql = "SELECT booking_id, user_id, book_date, book_slot FROM tbl_bookings";
-$result = $conn->query($sql);
+$result = $db->query($sql);
 
 if ($result->num_rows > 0) {
     echo "<table><tr><th>Booking ID</th><th>User ID</th><th>Date</th><th>Time</th></tr>";
@@ -36,7 +35,7 @@ if ($result->num_rows > 0) {
     echo "0 results";
 }
 
-$conn->close();
+$db->close();
 ?>
 
 </body>
